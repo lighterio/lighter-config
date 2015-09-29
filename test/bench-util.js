1f3aeb5039c7383d66cc56d5ff961ca3e458580c
Sam Eubank <sameubank+zerious@gmail.com>
2015-09-28 19:59:47 -0700

    Making benchmark alternatives optional

--- test/bench-util.js
+++ test/bench-util.js
@@ -3,7 +3,10 @@ var fns = {}
 var dir = __dirname + '/alternatives'
 fs.readdirSync(dir).forEach(function (file) {
   var key = file.replace('.js', '')
-  fns[key] = require(dir + '/' + file)
+  try {
+    fns[key] = require(dir + '/' + file)
+  } catch (ignore) {
+  }
 })
 
 exports.fns = fns
